4f7f20befd5fc3fca8c6010183f34b44138166b8
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-20 01:59:28 -0400

    adding aboutme component and details

---
+++ src/components/AboutMe/Index.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+const AboutMe = () => {
+  return (
+    <div className="aboutme">
+      <h1>About Me</h1>
+      <p>
+        “Whatever Your Mind Can Conceive and Believe, It Can Achieve.” –
+        Napoleon Hill. <br></br> I am a committed, service-minded and solution focused
+        Full Stack Developer and team player with a broad scope of experience in
+        OTA technology, hospitality, SaaS and digital marketing, with a passion
+        for learning code! <br></br> Unlike physics where physical restraints are in
+        everything you do, there are no limitations in coding besides your
+        ability to think of a logically consistent solution. As a life-long
+        learner, I'm driven by self-evolution and challenges, but more
+        importantly by acts of benevolence and servitude. With each new
+        programming language comes opportunity to create technologies to add
+        value, enrich or empower others, allowing me to live and work guided by
+        both mission and motivation. <br></br> As a GA Tech Bootcamp student, I'm quickly
+        learning JavaScript, HTML, CSS, jQuery, GitHub, Local Storage, APIs,
+        JSON, Bootstrap, MySQL and MERN stack as foundations for building
+        novice projects.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
