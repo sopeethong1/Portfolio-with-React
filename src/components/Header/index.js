0089b2ea279e111e599f0034206602fb2e6f5e59
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-24 01:42:28 -0400

    styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,32 +3,23 @@ import "./style.css";
 
 const Header = () => {
   return (
-   
-    <nav className="navbar">
-
-<img
-            style={{ height: "285px" }}
-            src="https://github.com/sopeethong1/Portfolio-with-React/blob/fca1df34599d9d8766a23ca88c38a601fdc23073/src/assets/headshot.png"
-            class="float-left pr-4 pb-3 img-fluid"
-            alt="SopeeHeadshot"
-          />
+    <header className="navbar">
           <ul>
-
               <li>
-                  <a href="/#about"> About</a>
+                  <a href="/#about">About</a>
               </li>
               <li>
-                  <a href="/#projects"> Projects</a>
+                  <a href="/#projects">Projects</a>
               </li>
               <li>
-                  <a href="/#contact"> Contact</a>
+                  <a href="/#contact">Contact</a>
               </li>
               <li>
                   <a href="/#resume">Resume</a>
               </li>
            
           </ul>
-      </nav>
+      </header>
   );
 };
 
