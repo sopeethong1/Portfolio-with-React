34290877b6e7fc312b9c0c2f38f3a06d97d437ec
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-24 03:48:37 -0400

    adding mdbreact again

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,16 +2,29 @@ import React from "react";
 // import "./style.css";
 const Footer = () => {
     return (
-      <div className="contact">
-      
-          <a href="tel:+15555555555">(913) 303-1681‬
-          </a>
-          <a href="mailto:tsopee@gmail.com">tsopee@gmail.com</a>
-          <a href="https://github.com/sopeethong1">GitHub</a>
-          <a href="https://www.linkedin.com/in/sopee-t-4a942363">LinkedIn</a>
-
+      <footer class="page-footer fixed-bottom font-small special-color-dark pt-4">
+      <div class="container">
+        <ul class="list-unstyled list-inline text-center">
+          <li class="list-inline-item">
+            <a
+              class="btn-floating btn-fb mx-1"
+              href="https://github.com/sopeethong1"
+            >
+              <i class="fab fa-github"> </i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a
+              class="btn-floating btn-li mx-1"
+              href="https://www.linkedin.com/in/sopee-t-4a942363"
+            >
+              <i class="fab fa-linkedin-in"> </i>
+            </a>
+          </li>
+        </ul>
       </div>
-    );
-  };
-  
+
+    </footer>
+  );
+}
   export default Footer;
